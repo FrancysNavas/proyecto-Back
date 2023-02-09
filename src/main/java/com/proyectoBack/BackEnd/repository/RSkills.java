15f20daf2c9f619bb0f9d910c9288a4642ec1669
FrancysNavas <francys.navas.sanchez@gmail.com>
2023-02-09 09:28:22 -0300

    Creacion de Repository

---
+++ src/main/java/com/proyectoBack/BackEnd/repository/RSkills.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.proyectoBack.BackEnd.repository;
+
+import com.proyectoBack.BackEnd.entity.Skills;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RSkills extends JpaRepository<Skills, Integer>{
+    
+    Optional<Skills> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
