4c9d1fa096e24724a843426a3312e0dc1abfd50e
FrancysNavas <francys.navas.sanchez@gmail.com>
2023-02-09 09:52:39 -0300

    Creacion DTO

---
+++ src/main/java/com/proyectoBack/BackEnd/dto/DtoProyectos.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyectoBack.BackEnd.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class DtoProyectos {
+    
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private String descripcion;
+   
+    private String img;
+
+    public DtoProyectos() {
+    }
+
+    public DtoProyectos(String nombre, String descripcion, String img) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.img = img;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
