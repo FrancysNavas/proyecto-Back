2504af9c0a146c8f1170b4cc397e201871f7e2a8
FrancysNavas <francys.navas.sanchez@gmail.com>
2023-03-24 22:43:09 -0300

    cambios en crossorigins

--- src/main/java/com/proyectoBack/BackEnd/controller/ExperienciaController.java
+++ src/main/java/com/proyectoBack/BackEnd/controller/ExperienciaController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explaboral")
-@CrossOrigin //(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendp-a0e71.web.app")
 public class ExperienciaController {
     
     @Autowired
